88958a5c83bc9e2a5ba05c732b53907bd8dca6f3
fnabilz <fikrizamzak@gmail.com>
2026-07-12 13:36:24 +0800

    updated get popular tv shows api to exclude animes (due to adult-content) for the time being

--- src/api/showApi.js
+++ src/api/showApi.js
@@ -2,7 +2,15 @@ import tmdb from "./tmdb"
 
 async function loadPopularShow() {
   try {
-    const response = await tmdb.get('/tv/popular')
+    const response = await tmdb.get('/discover/tv', {
+      params: {
+        include_adult: false,
+        include_null_first_air_dates: false,
+        language: 'en-US',
+        sort_by: 'popularity.desc',
+        with_origin_country: 'US|GB|KR|FR|ES|DE|CA'
+      }
+    })
     return response.data.results
   }
   catch (err) {
